b2c0cc6601eac275880d9e29f3725c7c1a40fd83
Samuel Silveira <samuel.silveira@live.com>
2026-09-01 11:24:27 -0300

    foundations/07_reverseString solved

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordArr = word.split('');
+    let reversedArr = [];
+    while (wordArr.length > 0) {
+        reversedArr.push(wordArr.pop());
+    }
+    return reversedArr.join('');
 };
 
 // Do not edit below this line

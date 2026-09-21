5407c941c797e54df9c98aa0f042a46a5da2154e
Samuel Silveira <samuel.silveira@live.com>
2026-09-21 18:05:04 -0300

    foundations/13_palindromes solved

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const stringArray = string
+        .replaceAll(" ", "")
+        .replaceAll(".", "")
+        .replaceAll(",", "")
+        .replaceAll("!", "")
+        .replaceAll("?", "")
+        .toLowerCase()
+        .split("");
+    
+    const invertedStringArray = [];
 
+    for (let i = 0; i < string.length; i++) {
+        invertedStringArray[string.length - i] = stringArray[i];
+    }
+
+    const formattedString = stringArray.join("");
+    const invertedString = invertedStringArray.join("");
+
+    return  formattedString === invertedString;
 };
 
 // Do not edit below this line

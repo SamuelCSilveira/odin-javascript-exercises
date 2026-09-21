c9359d4391aa7dcf8273982a5d318bee9394fd6a
Samuel Silveira <samuel.silveira@live.com>
2026-09-21 11:59:30 -0300

    foundations/12_calculator solved

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum += current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, current) => product *= current, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+	if (val === 0 || val === 1) return 1;
+  let result = val;
+  for (let i = val; i > 1; i--){
+    result = result * (i-1);
+  }
+  return result;
 };
 
 // Do not edit below this line

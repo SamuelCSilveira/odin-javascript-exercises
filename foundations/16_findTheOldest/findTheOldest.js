ff2235a8802a348d300c280557a3e6f0db457ee5
Samuel Silveira <samuel.silveira@live.com>
2026-09-22 13:50:05 -0300

    foundations/16_findTheOldest solved

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const CURRENT_YEAR = (new Date).getFullYear();
 
+        if (!oldest.yearOfDeath) oldest.yearOfDeath = CURRENT_YEAR;
+        if (!person.yearOfDeath) person.yearOfDeath = CURRENT_YEAR;
+
+        const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        const personAge = person.yearOfDeath - person.yearOfBirth
+
+        if (oldestAge < personAge) oldest = person;
+
+        return oldest;
+    }, people[0]);
 };
 
 // Do not edit below this line

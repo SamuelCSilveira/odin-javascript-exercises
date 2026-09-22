d06947a174eb80062e07b16bcae1abaaffbfe892
Samuel Silveira <samuel.silveira@live.com>
2026-09-22 11:27:24 -0300

    foundations/15_getTheTitles solved

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -2,11 +2,11 @@ const fibonacci = function(index) {
     if (Number(index) < 0) return "OOPS";
     if (Number(index) === 0) return 0;
 
-    const array = [ 1, 1 ];
+    const sequence = [ 1, 1 ];
     for (let i = 2; i <= Number(index); i++){
-        array.push(array[i-1] + array[i-2]); 
+        sequence.push(sequence[i-1] + sequence[i-2]); 
     }
-    return array[Number(index)-1];
+    return sequence[Number(index)-1];
 };
 
 // Do not edit below this line

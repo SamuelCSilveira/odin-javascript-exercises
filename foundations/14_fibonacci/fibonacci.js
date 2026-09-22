597ef52c194b59c4f22748600081e28abe92339c
Samuel Silveira <samuel.silveira@live.com>
2026-09-22 11:20:46 -0300

    foundations/14_fibonacci solved

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (Number(index) < 0) return "OOPS";
+    if (Number(index) === 0) return 0;
 
+    const array = [ 1, 1 ];
+    for (let i = 2; i <= Number(index); i++){
+        array.push(array[i-1] + array[i-2]); 
+    }
+    return array[Number(index)-1];
 };
 
 // Do not edit below this line
